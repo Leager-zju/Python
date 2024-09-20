1dddc3095e7d6aba1117270a17225554fc223451
Leager-zju <1004729740@qq.com>
2024-09-20 13:26:56 +0800

    update

--- sqlParser.py
+++ sqlParser.py
@@ -2,7 +2,7 @@ import sqlparse
 import re
 import json
 import re
-from typing import Dict, List, Optional
+from typing import Dict, List
 from enum import Enum
 
 class RefInfo:
@@ -30,10 +30,10 @@ class MetaData:
         self.attributes: List[str] = []
 
 class Relation:
-    def __init__(self, entityA:str, entityB:str) -> None:
+    def __init__(self, entityA:str, entityB:str, relation_type:RelationType = RelationType.NONE) -> None:
         self.A = entityA
         self.B = entityB
-        self.type = RelationType.NONE
+        self.type = relation_type
 
 class JsonData:
     def __init__(self) -> None:
@@ -44,15 +44,14 @@ class JsonData:
 
 def get_primary_key(column_def: str):
     if 'PRIMARY KEY' not in column_def:
-        return None
+        return None, False
 
     primary_key_column = re.search(r'PRIMARY\s+KEY\s+\((\w+)\)', column_def)
 
     if primary_key_column:
-        return primary_key_column.group(1)
+        return primary_key_column.group(1), False
 
-    primary_key = re.search(r'(\w+)', column_def).group(1)
-    return primary_key
+    return re.search(r'(\w+)', column_def).group(1), True
 
 def get_unique_key(column_def: str):
     if 'UNIQUE' not in column_def:
@@ -62,17 +61,19 @@ def get_unique_key(column_def: str):
     if unique_key_column:
         return unique_key_column.group(1)
     
-    unique_key = re.search(r'(\w+)', column_def).group(1)
-    return unique_key
+    return re.search(r'(\w+)', column_def).group(1)
 
 def get_reference_table(column_def: str):
     if 'FOREIGN KEY' not in column_def:
         return None, None
 
-    foreign_key_column = re.search(r'REFERENCES (\w+)\((\w+)\)', column_def)
+    foreign_key_column = re.search(r'REFERENCES\s+(\w+)\((\w+)\)', column_def)
 
     if foreign_key_column:
         return foreign_key_column.group(1), foreign_key_column.group(2)
+
+def get_normal_colname(column_def: str):
+    return re.search(r'(\w+)', column_def).group(1)
 
 def parse_sql(sql_statement):
     parsed = sqlparse.parse(sql_statement)
@@ -97,18 +98,21 @@ def parse_sql(sql_statement):
         for _, token in enumerate(tokens):
             txt = token.value
             if txt.startswith('('):
-                columns = txt[1 : txt.rfind(')')].replace('\n', '').split(',')
+                columns: List[str] = txt[1 : txt.rfind(')')].replace('\n', '').split(',')
                 for column in columns:
                     # case 1: primary key column
-                    primary_key = get_primary_key(column)
+                    primary_key, not_constraint = get_primary_key(column)
                     if primary_key:
                         table_info.primary_key = primary_key
+                        if not_constraint:
+                            table_info.columns.append(primary_key)
                         continue
 
                     # case 2: unique key column
                     unique_key = get_unique_key(column)
                     if unique_key:
                         table_info.uniques.append(unique_key)
+                        table_info.columns.append(unique_key)
                         continue
 
                     # case 3: foreign key column
@@ -120,32 +124,65 @@ def parse_sql(sql_statement):
                         continue
 
                     # case 4: normal column
-                    col_name = re.match(r'\b(\w+)\b', column)
-                    table_info.columns.append(col_name.group(1))
+                    col_name = get_normal_colname(column)
+                    if col_name:
+                        table_info.columns.append(col_name)
                 break
+
     return tables
 
 def generate_JSON(tables: Dict[str, TableInfo]):
-    data_list : List[MetaData] = []
+    json_data = {
+        'entities': [],
+        'relationships': []
+    }
+
     for table_name, table_info in tables.items():
-        data = MetaData(table_name)
+        entity_data = {
+            'name': table_name,
+            'attributes': [],
+        }
 
         for column in table_info.columns:
-            data.attributes.append(column)
+            entity_data['attributes'].append(column)
 
-        for reference in table_info.reference:
-            ref_table = reference.ref_table
-            ref_key = reference.ref_key
+        for i in range(len(table_info.reference)):
+            referenceA = table_info.reference[i]
+            ref_table_A = referenceA.ref_table
+            ref_key_A = referenceA.ref_key
 
-            relationship = Relation(ref_table, table_name)
-            if ref_key in tables[ref_table].uniques:
-                relationship.type = RelationType.ONE_TO_ONE
+            if ref_key_A in tables[ref_table_A].uniques:
+                json_data['relationships'].append(
+                    {
+                        'lhs': ref_table_A,
+                        'rhs': table_name,
+                        'type': 'one_to_one'
+                    }
+                )
+            else:
+                json_data['relationships'].append(
+                    {
+                        'lhs': ref_table_A,
+                        'rhs': table_name,
+                        'type': 'one_to_many'
+                    }
+                )
             
+                for j in range(i+1, len(table_info.reference)):
+                    referenceB = table_info.reference[j]
+                    ref_table_B = referenceB.ref_table
+                    ref_key_B = referenceB.ref_key
 
-            data.relationships.append(relationship)
+                    if ref_key_B == tables[ref_table_B].primary_key:
+                        json_data['relationships'].append(
+                            {
+                                'lhs': ref_table_A,
+                                'rhs': ref_table_B,
+                                'type': 'many_to_many'
+                            }
+                        )
         
-        data_list.append(data)
-    
-    json_data = json.dump({'ERD': data_list})
+        json_data['entities'].append(entity_data)
+
     with open('ERDFromPython.json', 'w+') as file:
-        file.write(json_data)
+        json.dump({'ERD': json_data}, file)
